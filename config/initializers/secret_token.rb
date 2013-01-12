09a84b77b93ecfd40fad4e2ea5d23c805f7d8178
saasbook <saasbook@saasbook.(none)>
2013-01-12 08:12:59 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarTrade2::Application.config.secret_token = 'fc11c19416555c80deb906dbc0210f9677c70d65de2f6af960590462aa8993ff26245ee5282cbd6672ce7d0cfd1a06515a5e56cb878f09f1020eb0604a303426'
